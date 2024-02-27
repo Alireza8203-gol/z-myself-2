d70025be1add28cc7816251c6d53d1d404e16547
Alireza8203-gol <alirezagolsorkhi1400@gmail.com>
2024-02-27 19:06:08 +0330

    left side of the resume section was created. this part was created NOT using ul

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,35 @@
 const $ = document;
 let navToggleBtn = $.querySelector(".nav__toggle-icon");
 let navMobileMenu = $.querySelector(".nav__mobile-menu");
+let resOptionCircleArr = $.querySelectorAll(".resume-option__circle");
 let isMobileMenuOpen = false;
 
 let toggleMobileMenu = () => {
-  navToggleBtn.classList.toggle('nav__toggle--open');
-
-  if (isMobileMenuOpen) {
-    // navToggleBtn.classList.remove("nav__toggle--open");
-    navMobileMenu.style.left = "-150%";
-    navMobileMenu.style.right = "auto";
-    isMobileMenuOpen = false;
-  } else {
-    // navToggleBtn.classList.add("nav__toggle--open");
-    navMobileMenu.style.left = "0";
-    navMobileMenu.style.right = "0";
-    isMobileMenuOpen = true;
-  }
+	navToggleBtn.classList.toggle("nav__toggle--open");
+	if (isMobileMenuOpen) {
+		navMobileMenu.style.left = "-150%";
+		navMobileMenu.style.right = "auto";
+		isMobileMenuOpen = false;
+	} else {
+		navMobileMenu.style.left = "0";
+		navMobileMenu.style.right = "0";
+		isMobileMenuOpen = true;
+	}
 };
 
+resOptionCircleArr.forEach((item) => {
+	item.parentElement.addEventListener("click", () => {
+    let resOpTitle = item.nextSibling.nextSibling.nextSibling.nextSibling;
+    console.log(resOpTitle);
+    resOptionCircleArr.forEach((input) => {
+      let resOpTitle = input.nextSibling.nextSibling.nextSibling.nextSibling;
+      input.style.width = "5rem"
+      resOpTitle.style.color = "#333333"
+    })
+    item.style.width = "100%";
+    resOpTitle.style.color = "#fff"
+  });
+});
+
 // navToggleBtn.addEventListener("toggle", toggleMobileMenu);
-navToggleBtn.addEventListener('click', toggleMobileMenu)
+navToggleBtn.addEventListener("click", toggleMobileMenu);
