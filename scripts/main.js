4f27ebd6477ee25ed7847172b198133436d622d8
Alireza8203-gol <alirezagolsorkhi1400@gmail.com>
2024-02-27 22:13:21 +0330

    structure and styles for the list on the left side was completed.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 const $ = document;
 let navToggleBtn = $.querySelector(".nav__toggle-icon");
 let navMobileMenu = $.querySelector(".nav__mobile-menu");
-let resOptionCircleArr = $.querySelectorAll(".resume-option__circle");
+let resOption = $.querySelectorAll(".resume-list__option");
+let resOptionCircles = $.querySelectorAll(".resume-list__circle");
 let isMobileMenuOpen = false;
 
 let toggleMobileMenu = () => {
@@ -17,18 +18,13 @@ let toggleMobileMenu = () => {
 	}
 };
 
-resOptionCircleArr.forEach((item) => {
-	item.parentElement.addEventListener("click", () => {
-    let resOpTitle = item.nextSibling.nextSibling.nextSibling.nextSibling;
-    console.log(resOpTitle);
-    resOptionCircleArr.forEach((input) => {
-      let resOpTitle = input.nextSibling.nextSibling.nextSibling.nextSibling;
-      input.style.width = "5rem"
-      resOpTitle.style.color = "#333333"
-    })
-    item.style.width = "100%";
-    resOpTitle.style.color = "#fff"
-  });
+resOption.forEach((option) => {
+	option.addEventListener("click", (e) => {
+		e.stopPropagation();
+		let circle = e.target;
+		console.log(circle);
+	}, true);
+	// console.log(option);
 });
 
 // navToggleBtn.addEventListener("toggle", toggleMobileMenu);
