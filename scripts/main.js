ecde05288e0207746dce9280562c270bcc165bd4
Alireza8203-gol <alirezagolsorkhi1400@gmail.com>
2024-03-22 21:57:54 +0330

    the portfolio section was completed

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,24 @@ let resOption = $.querySelectorAll(".resume-list__option");
 let portfolioCategoryItems = $.querySelectorAll(".portfolio-category__item");
 let isMobileMenuOpen = false;
 
+// Swiper module
+const swiper = new Swiper(".swiper", {
+	pagination: {
+		el: ".swiper-pagination",
+	},
+	breakpoints: {
+		992: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+		},
+		1400: {
+			slidesPerView: 3,
+			spaceBetween: 50,
+		},
+	},
+});
+
+// opening and closing the mobile hamburger menu
 let toggleMobileMenu = () => {
 	navToggleBtn.classList.toggle("nav__toggle--open");
 	if (isMobileMenuOpen) {
@@ -18,80 +36,61 @@ let toggleMobileMenu = () => {
 	}
 };
 
-resOption.forEach((option) => {
-	option.addEventListener("click", function () {
-		let tabId = this.id;
-		let tabsContent = $.querySelectorAll(`#${tabId}`);
-		let activeTab = $.querySelector(".resume-list__option--active");
-		let activeContent = $.querySelector(".resume-content--selected");
-		activeTab.classList.remove("resume-list__option--active");
-		this.classList.add("resume-list__option--active");
-		activeContent.classList.remove("resume-content--selected");
-		tabsContent[1].classList.add("resume-content--selected");
-	});
-});
-
+// resume section selection mechanism
+// resOption.forEach((option) => {
+// 	option.addEventListener("click", function () {
+// 		let tabId = this.getAttribute("tag");
+// 		let tabsContent = $.querySelector(`#${tabId}`);
+// 		let activeTab = $.querySelector(".resume-list__option--active");
+// 		let activeContent = $.querySelector(".resume-content--selected");
+// 		activeTab.classList.remove("resume-list__option--active");
+// 		this.classList.add("resume-list__option--active");
+// 		activeContent.classList.remove("resume-content--selected");
+// 		tabsContent.classList.add("resume-content--selected");
+// 	});
+// });
 portfolioCategoryItems.forEach((item) => {
-	item.addEventListener("click", () => {
-		let selectedTab = $.querySelector(".portfolio-category__item--active");
-		selectedTab.classList.remove("portfolio-category__item--active");
+	item.addEventListener("click", function () {
+		let itemTag = this.getAttribute("tag");
+		let tabsContent = $.querySelector(`#${itemTag}`);
+		let activeTab = $.querySelector(".portfolio-category__item--active");
+		let activeContent = $.querySelector(".portfolio-content--show");
+		activeTab.classList.remove("portfolio-category__item--active");
 		item.classList.add("portfolio-category__item--active");
+		activeContent.classList.remove("portfolio-content--show");
+		tabsContent.classList.add("portfolio-content--show");
 	});
 });
 
-// navToggleBtn.addEventListener("toggle", toggleMobileMenu);
-navToggleBtn.addEventListener("click", toggleMobileMenu);
-
-let cardsNodeList = $.querySelectorAll(".prototype");
-let cardsArray = Array.from(cardsNodeList);
-let allBtns = $.querySelectorAll(".select-btn");
-
-allBtns.forEach((btn) => {
-	btn.addEventListener("click", function (event) {
-		let btnTag = this.getAttribute("tag");
-		let filteredCards = cardsArray.filter((card) => {
-			return card.getAttribute("tag") === btnTag;
+// activating the selected tab and its related content
+function tabContentActivator(
+	// the array in which all the tab elements are stored
+	tabsArray,
+	// the class tha activates the clicked on tab
+	activatingTabClass,
+	// the class tha activates the clicked on content
+	activatingContentClass
+) {
+// to add the click event on each of the tab elements in the tabsArray
+	tabsArray.forEach((eachTab) => {
+		eachTab.addEventListener("click", function () {
+			// getting the value of the tag of each tab
+			let tabId = this.getAttribute("tag");
+			// selecting the related content of the selected tab
+			let tabsContent = $.querySelector(`#${tabId}`);
+			// selecting the active tab and its content
+			let activeTab = $.querySelector(`.${activatingTabClass}`);
+			let activeContent = $.querySelector(`.${activatingContentClass}`);
+			// deactivating the previous active tab and its content
+			activeTab.classList.remove(`${activatingTabClass}`);
+			activeContent.classList.remove(`${activatingContentClass}`);
+			// activating the clicked on (desired) tab and its content
+			this.classList.add(`${activatingTabClass}`);
+			tabsContent.classList.add(`${activatingContentClass}`);
 		});
-		console.log(filteredCards);
 	});
-});
-
-/* let imageArray = [
-	{id: 1, tag: "illustration", src: "./images/portfolio-left.jpg"},
-	{id: 2, tag: "animation", src: "./images/portfolio-middle.jpg"},
-	{id: 4, tag: "web-ui", src: "./images/portfolio-right.jpg"},
-	{id: 3, tag: "social-media", src: "./images/portfolio-left.jpg"},
-	{id: 5, tag: "illustration", src: "./images/portfolio-middle.jpg"},
-	{id: 6, tag: "print-design", src: "./images/portfolio-left.jpg"},
-	{id: 7, tag: "app-ui", src: "./images/portfolio-right.jpg"},
-	{id: 8, tag: "web-ui", src: "./images/portfolio-right.jpg"},
-	{id: 9, tag: "social-media", src: "./images/portfolio-middle.jpg"}
-];
-
-let currentPage = 1
-let imageCount = 3
-
-function sliderMachine() {
-	let cardTemplate = `
-	<div class="card">
-	<img src=${imageArray[1]} alt="portfolio" class="card__img">
-	</div>
-	`;
-} */
-
-let cardsCount = $.querySelectorAll(".card").length;
-const paginationWrapper = $.querySelector(".pagination");
-const cardsPerPage = 3;
-
-function paginationEventHandler (elem) {
-	elem.classList.add("resume-list__option--active");
 }
 
-for (let index = 0; index < cardsCount / cardsPerPage; index++) {
-	let paginationDiv = $.createElement("div");
-	paginationDiv.addEventListener('click', function () {
-		let activePage = $.querySelector(".pagination__div--active");
-		activePage.classList.remove("pagination__div--active")
-		paginationDiv.classList.add("pagination__div--active");
-	})
-}
+tabContentActivator(resOption, "resume-list__option--active", "resume-content--selected")
+tabContentActivator(portfolioCategoryItems, "portfolio-category__item--active", "portfolio-content--show")
+navToggleBtn.addEventListener("click", toggleMobileMenu);
