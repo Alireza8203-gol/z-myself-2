b3773f20aa3e4e28fa3fcbf5c79e27500077fdc9
Alireza8203-gol <alirezagolsorkhi1400@gmail.com>
2024-04-08 23:48:37 +0330

    scrolling function was implemented.

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,34 @@
 const $ = document;
-let navToggleBtn = $.querySelector(".nav__toggle-icon");
-let navMobileMenu = $.querySelector(".nav__mobile-menu");
-let resOption = $.querySelectorAll(".resume-list__option");
-let portfolioCategoryItems = $.querySelectorAll(".portfolio-category__item");
-let isMobileMenuOpen = false;
+const navToggleBtn = $.querySelector(".nav__toggle-icon");
+const navMobileMenu = $.querySelector(".nav__mobile-menu");
+const resOption = $.querySelectorAll(".resume-list__option");
+const portfolioCategoryItems = $.querySelectorAll(".portfolio-category__item");
+const menuItemsNodeList = $.querySelectorAll(".menu__link");
+const allSections = $.querySelectorAll("section");
+const isMobileMenuOpen = false;
 
 // Swiper module
 const swiper = new Swiper(".swiper", {
 	pagination: {
 		el: ".swiper-pagination",
 	},
+	spaceBetween: 30,
 	breakpoints: {
 		992: {
 			slidesPerView: 2,
-			spaceBetween: 30,
 		},
 		1400: {
 			slidesPerView: 3,
-			spaceBetween: 50,
 		},
 	},
 });
+
+const options = {
+	rootMargin: "10px",
+	threshold: 0.7,
+};
+
+const observer = new IntersectionObserver(observerCallback, options);
 
 // opening and closing the mobile hamburger menu
 let toggleMobileMenu = () => {
@@ -36,6 +44,13 @@ let toggleMobileMenu = () => {
 	}
 };
 
+function removeActiveClass(className) {
+	$.querySelector(`.${className}`).classList.remove(`${className}`);
+}
+function addClass(element, className) {
+	element.classList.add(`${className}`);
+}
+
 // activating the selected tab and its related content
 function tabContentActivator(
 	// the array in which all the tab elements are stored
@@ -45,27 +60,70 @@ function tabContentActivator(
 	// the class tha activates the clicked on content
 	activatingContentClass
 ) {
-// to add the click event on each of the tab elements in the tabsArray
+	// to add the click event on each of the tab elements in the tabsArray
 	tabsArray.forEach((eachTab) => {
 		eachTab.addEventListener("click", function () {
 			// getting the value of the tag of each tab
 			let tabId = this.getAttribute("tag");
 			// selecting the related content of the selected tab
 			let tabsContent = $.querySelector(`#${tabId}`);
-			// selecting the active tab and its content
-			let activeTab = $.querySelector(`.${activatingTabClass}`);
-			let activeContent = $.querySelector(`.${activatingContentClass}`);
+			// selecting the active tab and its content and
 			// deactivating the previous active tab and its content
-			activeTab.classList.remove(`${activatingTabClass}`);
-			activeContent.classList.remove(`${activatingContentClass}`);
+			removeActiveClass(activatingTabClass);
+			removeActiveClass(activatingContentClass);
 			// activating the clicked on (desired) tab and its content
-			this.classList.add(`${activatingTabClass}`);
-			tabsContent.classList.add(`${activatingContentClass}`);
+			addClass(this, activatingTabClass);
+			addClass(tabsContent, activatingContentClass);
 		});
 	});
 }
+
+function observerCallback(allSections) {
+	allSections.map((section) => {
+		// class name of the intersecting section
+		let sectionClassName = section.target.className;
+		if (section.isIntersecting) {
+			// selecting the li item that has the same data-section attr as the section class name
+			let sectionsMenuLink = $.querySelector(
+				`.menu__link[data-section=${sectionClassName}]`
+			);
+			// removing the active class from last element which is deactivated now
+			removeActiveClass("menu__link--active");
+			// adding the class to the activated element
+			addClass(sectionsMenuLink, "menu__link--active")
+		}
+	});
+}
+
+allSections.forEach(function (section) {
+	observer.observe(section);
+});
+
 // resume section tabs
-tabContentActivator(resOption, "resume-list__option--active", "resume-content--selected")
+tabContentActivator(
+	resOption,
+	"resume-list__option--active",
+	"resume-content--selected"
+);
 // portfolio section tabs
-tabContentActivator(portfolioCategoryItems, "portfolio-category__item--active", "portfolio-content--show")
+tabContentActivator(
+	portfolioCategoryItems,
+	"portfolio-category__item--active",
+	"portfolio-content--show"
+);
 navToggleBtn.addEventListener("click", toggleMobileMenu);
+
+menuItemsNodeList.forEach((menuItem) => {
+	menuItem.addEventListener("click", function (event) {
+		event.preventDefault();
+		removeActiveClass("menu__link--active");
+		addClass(menuItem, "menu__link--active");
+		let sectionClass = menuItem.getAttribute("data-section");
+		let sectionTopOffset = $.querySelector(`.${sectionClass}`).offsetTop;
+		window.scrollTo({
+			top: sectionTopOffset - 200,
+			behavior: "smooth",
+		});
+		console.log(sectionTopOffset);
+	});
+});
